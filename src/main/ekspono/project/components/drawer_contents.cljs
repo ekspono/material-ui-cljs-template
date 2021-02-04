dfeeb886727384062b938fe9cd63f10b9dd9d958
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2021-02-04 10:51:10 +0100

    improved example usages of mui components + comments

--- src/main/ekspono/project/components/drawer_contents.cljs
+++ src/main/ekspono/project/components/drawer_contents.cljs
@@ -11,18 +11,28 @@
 
 (def drawer-styles (withStyles (clj->js {:list {:width 250}
                                          :full-list {:width "auto"}})))
+
+(defn on-item-selected
+  [id]
+  (println "on-item-selected:" id))
+
 (defn drawer-list
   [{:keys [classes toggle_drawer] :as props}]
-  (println (js->clj props))
   [:div {:role "presentation"
          :class-name (.-list classes)
          :onClick #(toggle_drawer false)
          :onKeyDown #(toggle_drawer false)}
    [:> mui/List
-    [:> mui/ListItem
+    [:> mui/ListItem {:button true
+                      :onClick #(on-item-selected :choice-a)}
+     [:> mui/ListItemIcon
+      [:> mui-icons/Alarm]
+      [:> mui/ListItemText {:primary "Choice A"}]]]
+    [:> mui/ListItem {:button true
+                      :onClick #(on-item-selected :choice-b)}
      [:> mui/ListItemIcon
       [:> mui-icons/MoveToInbox]
-      [:> mui/ListItemText {:primary "Choice A"}]]]]])
+      [:> mui/ListItemText {:primary "Choice B"}]]]]])
 
 ;; TODO: r/reactify-component mangles the prop names
 ;; kebab-case turns into camelCase..
--- src/main/ekspono/project/components/main.cljs
+++ src/main/ekspono/project/components/main.cljs
@@ -8,12 +8,14 @@
             ["@material-ui/core/colors" :as mui-colors]
             [ekspono.project.components.drawer-contents :refer [drawer-contents]]))
 
+;; Example of how to make Material UI components interact with reagent state
 (def state (r/atom {:drawer-active false}))
 
 (defn toggle-drawer
   [active]
   (swap! state assoc :drawer-active active))
 
+;; An example of how to use Material UI's theme facility to override default colors
 (def custom-theme
   (createMuiTheme
    #js {:palette #js {:primary #js {:main (gobj/get (.-blue ^js/Mui.Colors mui-colors) 100)}}}))
@@ -28,6 +30,10 @@
 
 (def drawer-styles (withStyles (clj->js {:list {:width 250}
                                          :full-list {:width "auto"}})))
+
+;; The :> directive allows us to directly embed JS react components without
+;; first converting them using r/adapt-react-class, reducing the amount of 
+;; boilerplate code
 
 (defn app-bar
   [{:keys [classes] :as props}]
@@ -53,9 +59,12 @@
      [:> mui/Button {:color "inherit"}
       "Login"]]]])
 
+;; The app-bar component contains both the mui/AppBar and the mui/Drawer components
+;; Since app-bar is a reagent component it needs to be converted to a react component
+;; using r/reactify-component
+
 (defn main []
-  ;; fragment
-  [:<>
+  [:<> ;; return a react fragment
    [:> mui/CssBaseline]
    [:> mui/MuiThemeProvider
     {:theme custom-theme}
@@ -66,4 +75,8 @@
      [:> mui/Grid
       {:item true
        :xs 12}
-      [:> (with-app-bar-styles (r/reactify-component app-bar))]]]]])
+      [:> (with-app-bar-styles (r/reactify-component app-bar))]
+      [:> mui/Typography {:variant "h3"}
+       "Welcome to the project!"]
+      [:> mui/Typography {:variant "body1"}
+       "This is where you can put the main contents of the app"]]]]])
